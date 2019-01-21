f70eccdcaa4a1766505dfd463a3b95e850b1b715
Hyperreal <hyperreal@Hyperreals-Mini.home>
2019-01-20 23:20:51 -0500

    First Commit - Welcome Page HTML & CSS done (grid)

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-contain">
+    <div class="ab-content">
+    </div> <!-- AB Content End -->
+  </div> <!-- About Contain End -->
+</template>
+
+<script>
+export default {
+  name: 'about',
+};
+</script>
+
+<style lang="scss" scoped>
+.about-contain {
+  height: 800px;
+  background-color: #fff;
+  z-index: 1;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="home-contain">
+    <div class="hm-content">
+      <h1>Team Design and <br>Decoration</h1>
+      <div class="nav-1">
+        <a href="#">Contact</a>
+        <a href="#">Instagram</a>
+      </div> <!-- Nav-1 End -->
+      <div class="nav-2">
+        <ul>
+          <li class="circle"> <a href="#"></a> </li>
+          <li class="circle"> <a href="#"></a> </li>
+          <li class="circle"> <a href="#"></a> </li>
+        </ul>
+      </div> <!-- Nav-2 End -->
+      <div class="arrow"><i class="fas fa-chevron-circle-down"></i></div>
+    </div> <!-- HM Content End -->
+  </div> <!-- Home Contain End -->
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
+
+<style lang="scss" scoped>
+.home-contain {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/img/cover/cover-img.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50%;
+  .hm-content {
+    display: grid;
+    grid-template-columns: 1fr 5fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas:
+      "bl-left  headline  nav-1"
+      "bl-left  headline  nav-2"
+      "footer   footer    footer";
+    margin: 30px;
+    h1 {
+      font-family: var(--headline-font);
+      font-variant: small-caps;
+      font-size: 60px;
+      font-weight: 100;
+      letter-spacing: 5px;
+      text-align: center;
+      color: #fff;
+      text-shadow: 0px 2px 13px rgba(0, 0, 0, 0.3);
+      grid-area: headline;
+    }
+    .nav-1 {
+      grid-area: nav-1;
+      padding: 10px 0;
+      a {
+        font-family: var(--headline-font);
+        text-decoration: none;
+        letter-spacing: 5px;
+        padding: 10px 25px;
+        color: #fff;
+        &:hover {
+          color: red;
+        }
+      }
+    }
+    .nav-2 {
+      grid-area: nav-2;
+      display: grid;
+      align-content: center;
+      ul {
+        display: grid;
+        justify-content: end;
+        padding: 0;
+        list-style: none;
+        .circle {
+          border: 1.5px solid #fff;
+          border-radius: 50px;
+          margin: 10px;
+          padding: 5px;
+        }
+      }
+    }
+    .arrow {
+      display: grid;
+      justify-content: center;
+      align-items: end;
+      grid-area: footer;
+      font-size: 50px;
+      color: rgba(255, 255, 255, 0.5);
+    }
+  }
+}
+</style>
