a2dbc2243c26150ce7a735282be73189313e2ce6
Hyperreal <hyperreal@Hyperreals-Mini.home>
2019-01-26 01:09:41 -0500

    commented out data section in Projects

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -2,7 +2,7 @@
   <div class="projects-contain">
     <div class="proj-content">
       <h2>Our Projects</h2>
-      <div class="projects">
+      <div class="project">
       </div>
     </div> <!-- Projects Content End -->
   </div> <!-- Projects Container End -->
@@ -12,6 +12,30 @@
 
 export default {
   name: 'projects',
+  // data() {
+  //   proj: [
+  //     {
+  //       id: 1,
+  //       title: 'Arizona Retreat',
+  //       img_url: 'proj-img/plc-hld-img/az-ret.jpg',
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'Chelsea Loft',
+  //       img_url: 'proj-img/plc-hld-img/ch-loft.jpg',
+  //     },
+  //     {
+  //       id: 3,
+  //       title: 'Coastal Connecticut',
+  //       img_url: 'proj-img/plc-hld-img/cost-ct.jpg',
+  //     },
+  //     {
+  //       id: 4,
+  //       title: 'Greenwich Village Pied a Terre',
+  //       img_url: 'proj-img/plc-hld-img/gv-pdt.jpg',
+  //     },
+  //   ],
+  // }
 };
 </script>
 
