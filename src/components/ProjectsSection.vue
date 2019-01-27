885ad7d56f09f164bceebf2873ab5dd2f1c39bd8
Rivera <nrivera@admins-MBP-2.frontierlocal.net>
2019-01-26 20:08:30 -0500

    Pure JS to Vue JS

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -2,40 +2,51 @@
   <div class="projects-contain">
     <div class="proj-content">
       <h2>Our Projects</h2>
-      <div class="project">
+
+      <div v-for="(project, idx) in data.default" :key="idx">
+        <div class="item h${randomNumber(4)} v${randomNumber(4)}" @click="projectClick()">
+          <img :src="`img/proj-img/plc-hld-img/${chooseImage()}.jpg`">
+          <div class="item__overlay">
+            <button>View →</button>
+          </div>
+        </div>
       </div>
-    </div> <!-- Projects Content End -->
-  </div> <!-- Projects Container End -->
+
+      <div class="project"></div>
+    </div>
+    <!-- Projects Content End -->
+  </div>
+  <!-- Projects Container End -->
 </template>
 
 <script>
+import * as data from "../data.json";
 
 export default {
-  name: 'projects',
-  // data() {
-  //   proj: [
-  //     {
-  //       id: 1,
-  //       title: 'Arizona Retreat',
-  //       img_url: 'proj-img/plc-hld-img/az-ret.jpg',
-  //     },
-  //     {
-  //       id: 2,
-  //       title: 'Chelsea Loft',
-  //       img_url: 'proj-img/plc-hld-img/ch-loft.jpg',
-  //     },
-  //     {
-  //       id: 3,
-  //       title: 'Coastal Connecticut',
-  //       img_url: 'proj-img/plc-hld-img/cost-ct.jpg',
-  //     },
-  //     {
-  //       id: 4,
-  //       title: 'Greenwich Village Pied a Terre',
-  //       img_url: 'proj-img/plc-hld-img/gv-pdt.jpg',
-  //     },
-  //   ],
-  // }
+  name: "projects",
+  data() {
+    return {
+      data,
+      picked: []
+    };
+  },
+  methods: {
+    randomNumber(limit) {
+      return Math.floor(Math.random() * limit) + 1;
+    },
+    chooseImage() {
+      const a = this.randomNumber(4);
+      // if (this.picked.includes(a)) {
+      //   a = this.randomNumber(4);
+      // }
+
+      this.picked.push(a);
+      console.log("herro");
+    },
+    projectClick(route) {
+      // $route.push(route);
+    }
+  }
 };
 </script>
 
@@ -44,8 +55,7 @@ export default {
   width: 100vw;
   display: grid;
   grid-template-columns: 1fr 5fr 1fr;
-  grid-template-areas:
-    ".  content   .";
+  grid-template-areas: ".  content   .";
   .proj-content {
     grid-area: content;
   }
