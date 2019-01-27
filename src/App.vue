bb32d73227ba73089c041eabd6b8e5422d57f776
Rivera <nrivera@admins-MBP-2.frontierlocal.net>
2019-01-26 21:27:25 -0500

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
-    <AboutSection />
-    <ProjectsSection />
+    <router-view/>
+    <AboutSection/>
+    <ProjectsSection/>
   </div>
 </template>
 
--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -4,8 +4,10 @@
       <h2>About</h2>
       <!--eslint-disable-->
       <p>Rick Garofalo has been a champion in the design industry in the United States since the late 80’s. Founded in 1988 by Rick, repertoire became one of the premier design ateliers in the United States bringing the worlds best design to Boston and eventually New York. Repertoire created residential and commercial projects of great distinction until 2005. Since 2005, originally based in New York City, reinvented as Team Design and Decoration has worked on projects in New York City, The Hamptons, Westchester County Florida and Arizona. In 2015, having done a project in Rowayton, Connecticut, Rick fell in love with coastal Connecticut and moved the design office to Stamford, an easy commute to service New York City, Westchester and Fairfield Counties. With almost 30 years in the design industry, Rick is known for his creativity, his hands on attention to detail, and sense of humor.</p>
-    </div> <!-- AB Content End -->
-  </div> <!-- About Contain End -->
+    </div>
+    <!-- AB Content End -->
+  </div>
+  <!-- About Contain End -->
 </template>
 
 <script>
@@ -21,8 +23,7 @@ export default {
   background-color: #fff;
   display: grid;
   grid-template-columns: 1fr 3fr 1fr;
-  grid-template-areas:
-    ".  content   .";
+  grid-template-areas: '.  content   .';
   align-items: center;
   .ab-content {
     display: grid;
@@ -38,7 +39,8 @@ export default {
       text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5);
     }
     p {
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
       letter-spacing: 2px;
       font-size: 17px;
       line-height: 50px;
